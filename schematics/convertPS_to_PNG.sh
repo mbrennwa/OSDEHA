dd5af471bab0944c3ea897038e32c4a594a49edd
Matthias Brennwald <matthias@brennwald.org>
2024-01-12 17:51:27 +0100

    add schem

--- schematics/convertPS_to_PNG.sh
+++ schematics/convertPS_to_PNG.sh
@@ -4,5 +4,5 @@ f=$1
 b="${f%.*}"
 echo "$b"
 
-gs -dSAFER -r600 -dEPSCrop -sDEVICE=pnggray -dDownScaleFactor=4 -o "$b".png "$f" && convert -negate "$b".png "$b".png
+gs -dSAFER -r900 -dEPSCrop -sDEVICE=pnggray -dDownScaleFactor=4 -o "$b".png "$f" && convert -negate "$b".png "$b".png
 
