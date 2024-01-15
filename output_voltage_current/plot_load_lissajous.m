f6390b62200df00eca9fa8ef8cd41a7e0bbdf68c
Matthias Brennwald <mbrennwa@gmail.com>
2024-01-15 13:32:17 +0100

    mu follower doc

--- output_voltage_current/plot_load_lissajous.m
+++ output_voltage_current/plot_load_lissajous.m
@@ -10,21 +10,30 @@
 % applied voltage: U = U0 x sin(2pi f t) --> dU/dt = 2pi x f x U0 x cos(2pi f t)
 % --> I = C x 2pi x f x U0 x cos(2pi f t)
 
-ff = [1E3,3e3,1e4,3e4,1e5];
-C  = 100e-12;
-U0 = 500; % Voltage amplitude (Vpk)
+C  = 100e-12; % capacitance in Farads
+% Ra = 66E3;    % AC resistance of anode resistor/CCS in Ohms
+Ra = 10E6;    % AC resistance of anode resistor/CCS in Ohms
+
+U0 = 300; % Voltage amplitude (Vpk)
 
 % DC bias point:
 Ubias = 400;
 Ibias = 20E-3;
 
+
+ff = [1E3,3e3,1e4,3e4,1e5];
+
+
+
 for f=ff
 	t  = linspace(0,1/f,300);
 	U  = U0 * sin(2*pi*f*t);
-	I  = C * 2*pi * f * U0 * cos(2*pi*f*t);
-	plot(U+Ubias,1000*(I+Ibias),'r-','linewidth',1); hold on
+	IC = C * 2*pi * f * U0 * cos(2*pi*f*t);
+	IR = U / Ra;
+	I  = IC + IR;
+	plot(U+Ubias,1000*(-I+Ibias),'r-','linewidth',1); hold on
 end
-r = axis();
+r = axis(); r(1) = 0; r(3) = 0; axis(r) 
 plot([r(1),Ubias],[Ibias,Ibias]*1000,'b-','linewidth',2);
 plot([Ubias,Ubias],[r(3),Ibias*1000],'b-','linewidth',2);
 plot(Ubias,Ibias*1000,'.','color','b','markersize',20);
@@ -32,4 +41,4 @@ hold off
 
 xlabel('Voltage (V)')
 ylabel('Current (mA)')
-title(sprintf('Capacitive I-V Load Curves, Sine Signals at f = %s kHz',strrep(sprintf('%.0f,' , ff/1000)(1:end-1),',',', ')));
+title(sprintf('I-V Load Curve from Anode Load R + Capacitor Load\\n Sine Signals at f = %s kHz',strrep(sprintf('%.0f,' , ff/1000)(1:end-1),',',', ')));
