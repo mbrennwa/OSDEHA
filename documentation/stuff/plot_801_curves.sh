3b0743979addaada8ae439a9dee4550c9464f62d
Matthias Brennwald <matthias@brennwald.org>
2024-05-21 01:38:05 +0200

    801 curves

---
+++ documentation/stuff/plot_801_curves.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curveplot 801A_2.dat --xlabel 'ANODE-CATHODE VOLTAGE' --ylabel 'ANODE CURRENT' --yscale m --ylimit 0.028 --title '801A' --width 16 --height 8 --fontname 'Helvetica' --nobranding --savepdf
